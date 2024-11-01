03e2670d61bc6f47ba83962beb08b609c56b9321
maalin2 <1maalinma@gmail.com>
2024-11-01 19:50:02 -0400

    start server

--- server/team-13-cfg/backend/server.js
+++ server/team-13-cfg/backend/server.js
@@ -2,4 +2,11 @@ import express from 'express';
 import axios from 'axios';
 import dotenv from 'dotenv';
 import { extractTextFromPDF } from './pdfHandler.js';
+import { appendFile } from 'fs';
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
 
