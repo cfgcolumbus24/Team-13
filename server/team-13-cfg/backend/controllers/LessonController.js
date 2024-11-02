3bce6c7ecf836e9e2caf18110eede9dec2dac468
maalin2 <1maalinma@gmail.com>
2024-11-02 03:17:08 -0400

    message

--- server/team-13-cfg/backend/controllers/LessonController.js
+++ server/team-13-cfg/backend/controllers/LessonController.js
@@ -8,7 +8,7 @@ export const processLesson = async (req, res) => {
         }
 
         const lessonPlan = await processLessonPlan(prompt);
-        console.log("Generated Lesson Plan:", lessonPlan); 
+        console.log(lessonPlan.data);
         res.status(200).json({ success: true, data: lessonPlan });
     } catch (error) {
         console.error('Error processing lesson plan:', error);
