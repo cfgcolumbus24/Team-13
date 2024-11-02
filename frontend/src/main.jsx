4d686cbd6588f3c5506057677a85817b1b54d13e
amaya-j <164046047+amaya-j@users.noreply.github.com>
2024-11-01 22:43:18 -0400

    completed-frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter,Routes, Route, Link } from 'react-router-dom';
 import './index.css'
-import NewsletterPage from './components/NewsletterPage';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -11,15 +8,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <nav>
-      <Link to="/">Home</Link> | <Link to="/newsletter">Newsletter</Link>
-    </nav>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/newsletter" element={<NewsletterPage />} />
-    </Routes>
-  </BrowserRouter>
-);
